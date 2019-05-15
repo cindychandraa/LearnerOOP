d3844c275770988a755aacfe5b654cbd5ea93e60
Cindy Chandra <cxndych@gmail.com>
2019-05-15 14:19:01 +0700

    Project DONE. I'm genius 🤪

--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -9,11 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var learnerGenderLabel: UILabel!
+    @IBOutlet weak var learnerAgeLabel: UILabel!
+    @IBOutlet weak var learnerNameLabel: UILabel!
+    
+    var learnerInstance: LearnerModel? // You just created a copy of your LearnerModel
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        learnerInstance = LearnerModel(nameLearner: "Jason", ageLearner: 21, genderLearner: "Male", imageProfileLearner: "")
+        updateUI()
     }
 
+    func updateUI() {
+        if let instance = learnerInstance {
+            learnerNameLabel.text = instance.name
+            learnerAgeLabel.text = "\(instance.age)"
+            learnerGenderLabel.text = instance.gender
+        }
+    }
 
+    @IBAction func btnAgePress(_ sender: UIButton) {
+        if let instance = learnerInstance {
+            instance.increaseAge()
+        }
+        updateUI()
+    }
 }
 
